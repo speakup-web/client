5e2a60313266a919f82a152ac1c718d0980a308a
tiarapr <tiaraputriramadhani1@gmail.com>
2024-03-23 01:42:41 +0700

    Update laporan terkirim

--- src/components/LaporanTerkirim.jsx
+++ src/components/LaporanTerkirim.jsx
@@ -3,19 +3,19 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 export function LaporanTerkirim() {
     return (
-    <section className="max-w-screen-lg mx-auto mt-20 px-4 sm:px-6" id='LaporanTerkirim1'>
-        <h1 className="text-4xl font-bold tracking-tight text-blue-950 sm:text-6xl text-center font-popins">
+    <section className="max-w-screen-lg mx-auto mt-20 px-4 sm:px-6 space-y-5" id='LaporanTerkirim1'>
+        <h1 className="xl:text-4xl tracking-tight text-blue-950 sm:text-5xl text-center poppins-semibold">
             Pengaduan Terkirim
         </h1>
         <div className="flex justify-center items-center mt-5">
             <img src={LogoLaporan} alt="Mail" className="md:w-auto max-w-60 h-auto" />
         </div>
-        <h1 className="text-4xl mt-4 font-bold tracking-tight text-black sm:text-2xl text-center josefin-sans-regular">
+        <h1 className="xl:text-2xl mt-4 font-bold tracking-tight text-black sm:text-2xl text-center noto-sans-thin">
             Cek email kamu atau bisa langsung klik link berikut:
         </h1>
         <div className='mt-3 mb-10 flex justify-center items-center'>
-            <button type="submit" className="text-white hover:bg-blue-300 focus:ring-4 focus:outline-none font-medium rounded-full text-sm flex items-center justify-center w-full sm:w-auto px-20 py-2.5 my-4 mr-4 text-center dark:hover:bg-blue-600 bg-blue-500">
-                Pantau Pengaduan <FaArrowRightLong className="ml-1 mt-1" />
+            <button type="submit" className="text-white hover:bg-blue-300 focus:ring-4 focus:outline-none font-medium rounded-full text-sm flex items-center justify-center w-full sm:w-auto px-20 py-3 my-4 mb-20 mr-4 text-center dark:hover:bg-blue-600 bg-blue-500 poppins-semibold">
+                Pantau Pengaduan <span><FaArrowRightLong className="ml-3" /></span>
             </button>
         </div>
     </section>
